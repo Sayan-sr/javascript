21ea72ff4cb092ab21289fd2da0c34634d6c1221
Sayan-sr <sayansantra2010@gmail.com>
2025-06-19 20:51:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3 
+    +
+    3
+); // code readability should be high
+
+console.log("Sayan");
+
+let name = "Sayan"
+let age = 21
+let isLoggedIn = false
+
+
+// primitive data types
+
+// number => 2 to power  53
+// BigInt
+// String => ""
+// Boolean => true/false 
+// null => standalone value 
+// undefined => value not yet assigned 
+// symbol = unique
+
+
+// object
+
+console.log(typeof "Sayan");
+console.log(typeof age);
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); //undefined
+
